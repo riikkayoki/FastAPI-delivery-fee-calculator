9b1134534c2aaf85b43e67c95e4bc5923d16daf9
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2022-11-18 16:13:41 +0200

    request body BaseModel increment

--- app/main.py
+++ app/main.py
@@ -1,4 +1,14 @@
+from datetime import datetime
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# BaseModel by pydantic creates a request body request body
+# that sends data by the client to the API.
+class Request(BaseModel):
+	cart_value: int
+	delivery_distance: int
+	number_of_items: int
+	time: datetime
 
 app = FastAPI(title="Delivery Fee Calculator API")
 
@@ -7,5 +17,5 @@ async def home():
     return {"Welcome to": "FastAPI Delivery Fee Calculator"}
 
 @app.post("/calculator")
-async def calculator():
-    return {"This will be the": "POST calculator"}
+async def calculator(request: Request):
+    return request
